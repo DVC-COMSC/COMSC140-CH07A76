d32bd15755447214cf28442c9c25825226ebd893
Kyuwoong Lee <kwooler@gmail.com>
2022-10-01 14:12:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 
-import sys
-
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
 
-if len(num1) > len(num2):
-	print ('False')
-	sys.exit(0)
 # ******************************
 # Make your Code
 # ******************************
 
-# print ('True') or ('False')
+# print (num3) 
